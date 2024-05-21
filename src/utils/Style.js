2a796c590c0693b18b249532144d4d230199ad03
Faizanniaz03 <faizanniaz03@gmail.com>
2024-05-21 17:22:35 +0500

    height change

--- src/utils/Style.js
+++ src/utils/Style.js
@@ -22,7 +22,7 @@ const Styles = StyleSheet.create({
       PrayerTimes2:{
         width: '90%',
         marginTop:20,
-        height: '48%',
+        height: '42%',
         backgroundColor: '#e2ddd9',
         alignSelf: 'center',
         borderRadius: 20,
